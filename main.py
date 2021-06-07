3ed5e0de3bbd41b2253ed5c963d122b2206e492a
Muhammadqodir <m.qodir777@gmail.com>
2021-06-07 03:12:14 +0300

    Added Categories List

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 import telebot
 import config
+from telebot import types
 
 TOKEN = config.BOT_API
 
-tb = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
+categorys = [
+	"Автомобильный бизнес",
+	"Банки",
+	"Безопасность",
+	"Бухгалтерия",
+	"Менеджмент",
+	"Домашний персонал",
+	"Закупки",
+	"Информационные технологии",
+	"Консультирование",
+	"Маркетинг",
+	"Медицина",
+	"Наука образование",
+	"Рабочий персонал",
+	"Страхование",
+	"Транспорт",
+	"Туризм",
+	"Юристы"
+]
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "My Job Поиск ваканций")
+	markup = types.ReplyKeyboardMarkup()
+	search = types.KeyboardButton('Поиск 🔍')
+	markup.row(search)
+	for x in range(0,len(categorys)):
+		item = types.KeyboardButton(categorys[x])
+		markup.row(item)
+		pass
+	bot.send_message(message.chat.id, "Выберите категорию:", reply_markup=markup)
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+
+bot.polling()
